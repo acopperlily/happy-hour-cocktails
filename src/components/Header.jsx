dda60ed559e747bc95d55f77dd13531f0c3003b7
acopperlily <april.copley@protonmail.com>
2023-02-01 13:26:43 -0500

    Troubleshooting deplyment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { FaInfoCircle } from 'react-icons/fa';
-import cocktails from '../assets/singleCocktail.png';
 
 const Header = props => {
   return (
     <header>
-      {/* <img src={cocktails} alt=""/> */}
       <h1 className="logo">Happy Hour Cocktails</h1>
       <div className="infoContainer">
         <FaInfoCircle className="info" />
